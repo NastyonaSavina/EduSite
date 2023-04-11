bddbba06a0fa99d386f573c9cab3a034fb54381c
NastyonaSavina <103425739+NastyonaSavina@users.noreply.github.com>
2023-04-11 00:20:27 +0000

    Deploying to gh-pages from @ NastyonaSavina/EduSite@ae3af0f93a97b256f646263b823a1a28fa2a8217 🚀

---
+++ static/js/90.72ed1ba8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[90],{90:function(e,t,a){a.r(t);t.default=function(){}}}]);
+//# sourceMappingURL=90.72ed1ba8.chunk.js.map
