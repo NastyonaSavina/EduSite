0ade62bc56e2791ea9b43ffc539d43368975a9bb
NastyonaSavina <nastyona.savina@gmail.com>
2023-04-13 15:24:00 +0300

    fixed image bug

--- src/components/Main/CoursesList/CourseItem.jsx
+++ src/components/Main/CoursesList/CourseItem.jsx
@@ -1,9 +1,14 @@
 import './CoursesList.css';
 
+import imagecourse from '../../../images/coursesImages/physics2.png'
+
+
+//temporary use one image because of lack good images by url for json base
+
 export const CourseItem = ({ title, description, imageUrl }) => {
   return (
     <li className="course-item">
-      <img className="course-foto" src={imageUrl} alt={title} />
+      <img className="course-foto" src={imagecourse} alt={title} />
       <h3 className="course-title">{title}</h3>
       <p>{description}</p>
     </li>
